2cb43004d81a745598df7d22f3ae3a70a28a49e5
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-01 11:53:51 +0500

    elif task 03

--- if03.py
+++ if03.py
@@ -8,4 +8,11 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a>b and a<c or a<b and a>c:
+        return a
+    elif b>a and b<c or b<a and b>c:
+        return b
+    else:
+        return c 
+    
+print(main(1,-1,0))
